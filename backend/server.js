9adc508b3957bbff74f8f8a66c1230c9cdb136c5
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-02-22 18:04:11 -0500

    chalk

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 
-const express = require("express");
-
+import express from "express";
+import chalk from 'chalk'
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(
+        chalk.blueBright("Server is listening on PORT:"),
+        chalk.yellow(PORT),);
 });
 
