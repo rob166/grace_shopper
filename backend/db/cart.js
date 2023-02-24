1267a4c7cdf9747f151cf19e16bff980842a7ae0
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-02-23 21:37:02 -0500

    some left over commits

--- backend/db/cart.js
+++ backend/db/cart.js
@@ -3,16 +3,6 @@ const bcrypt = require('bcrypt')
 const SALT_COUNT = 10;
 
 
-const getAllItemsInCart = async (cartId) => {
-    try {
-        const { rows: cart } = await client.query(`
-        SELECT * FROM products
-        WHERE cart_id=$1;`, [cartId]);
-        return cart
-    } catch (error) {
-        console.error(error)
-    }
-};
 
 // const updateCart = async 
 
