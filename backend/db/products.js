299e025bc2e5ad9dabda877e5a6d4f5d933dd1f5
carowoline <caro98.jpg@gmail.com>
2023-02-23 21:11:35 -0500

    created getAllProducts function inside products.js

--- backend/db/products.js
+++ backend/db/products.js
@@ -12,14 +12,27 @@ const createProduct = async ({ name, description, price, image, cart_id }) => {
           VALUES($1,$2,$3,$4,$5)  
           RETURNING *;`, [name, description, price, image, cart_id]);
             return product;
-      } catch (error) { console.warn(error) }
-}
+      }
+      catch (error) {
+            console.warn(error)
+      }
+};
 
+const getAllProducts = async () => {
+      try {
+            const { rows: products } = await client.query(`
+            SELECT *
+            FROM products
+            `);
 
-
-
-
+            return products;
+      }
+      catch (error) {
+            throw new Error("Cannot get all products")
+      }
+};
 
 module.exports = {
-      createProduct
+      createProduct,
+      getAllProducts,
 }
