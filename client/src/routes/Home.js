f7c61aa51c0be0385f5c3f455ffc4fcec45c3e0c
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-02-27 19:32:45 -0500

    commits

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { showProducts } from "../Api.fetches";
+import HomeCss from '../css/Home.module.css'
 const Home =()=>{
     const [products,setProducts] = useState([]);
     const showProd = async()=>{
@@ -10,9 +11,16 @@ const Home =()=>{
         showProd()
  },[])
 
-    return(products?products.map(p=>  <div>
-            {p.name}
-        </div>):<div>no work</div>
+    return(<div className={HomeCss.body}>
+       { products?products.map(p=>  
+    <div className={HomeCss.product}key={crypto.randomUUID()}>
+    <h2> {p.name}</h2>
+    <div className={HomeCss.imgDiv}>
+    <img className={HomeCss.img} src={require(`../img/${p.image}`)} alt='drink'></img>
+    </div>
+    </div>)
+    :<div>no work</div>}
+    </div>
       
     )
 }
--- client/src/routes/Landing.js
+++ client/src/routes/Landing.js
@@ -2,18 +2,18 @@ import { useState } from 'react'
 import { Navigate } from 'react-router'
 import LandingCss from '../css/Landing.module.css'
 
-const Landing = () =>{
-    const [adult,setAdult] = useState(false)
-    return(adult?<Navigate to='/home'/>:
-        <div className={LandingCss.body}> 
-        <div><h1 className={LandingCss.header}>Grace Shopper buzzed</h1></div>
-        <div className={LandingCss.answerBox}>
-        <h1 className={LandingCss.answerBoxText}>Are you 21 or older?</h1>
-            <div className={LandingCss.buttons}>
-                <button className={LandingCss.button} onClick={()=>setAdult(true)}>Yes</button>
-                <button className={LandingCss.button}>NO</button>
+const Landing = () => {
+    const [adult, setAdult] = useState(false)
+    return (adult ? <Navigate to='/home' /> :
+        <div className={LandingCss.body}>
+            <div><h1 className={LandingCss.header}>Grace Shopper buzzed</h1></div>
+            <div className={LandingCss.answerBox}>
+                <h1 className={LandingCss.answerBoxText}>Are you 21 or older?</h1>
+                <div className={LandingCss.buttons}>
+                    <button className={LandingCss.button} onClick={() => setAdult(true)}>Yes</button>
+                    <button className={LandingCss.button}>NO</button>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
