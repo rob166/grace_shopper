1ef4c640c05bd18876c654f091d498706a8018d6
jessanahi <jess.anahi.zamora@gmail.com>
2023-03-01 19:04:28 -0600

    cleaning my branch

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './css/index.css';
-=======
->>>>>>> 04b51caef53a1223c3bc5cdd7ad6e34a4c0eec31
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
