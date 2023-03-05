f6c6a27d0f880734b167d5aff6dd7e5d768c20b6
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-03-04 20:52:35 -0500

    cleaning up code

--- client/src/routes/Cart.js
+++ client/src/routes/Cart.js
@@ -7,7 +7,7 @@ const Cart = ({ cookie }) => {
 
     const [cart, setCart] = useState([])
     const [edit, setEdit] = useState(false)
-    const [render,setRender]=useState(null)
+    const [render, setRender] = useState(null)
 
     const getCartItems = async () => {
 
@@ -16,8 +16,6 @@ const Cart = ({ cookie }) => {
         setCart(cartItems)
     }
 
-   
-
     useEffect(() => {
         getCartItems()
         // eslint-disable-next-line
@@ -25,29 +23,54 @@ const Cart = ({ cookie }) => {
 
     return (
         <div className={CartCss.body}>
-            {edit ? <div className={CartCss.editPage}>  <PriceChanger setEdit={setEdit} setRender={setRender} cookie={cookie}/></div> : null}
-            <form className={CartCss.container}>
+
+            {edit ?
+                <div className={CartCss.editPage}>
+                    <PriceChanger
+                        setEdit={setEdit}
+                        setRender={setRender}
+                        cookie={cookie} />
+                </div> : null}
+
+            <form
+                className={CartCss.container}>
+                
                 {cart ? cart.map(p => p.quantity > 0 ?
-                    <div 
+                    <div
                         className={CartCss.productDiv}
                         key={crypto.randomUUID()}
                     >
-                        <div className={CartCss.name}>{p.name}<div>
+                        <div
+                            className={CartCss.name}>{p.name}<div>
 
-                            <button className={CartCss.editButton} onMouseOver={()=>cookie.set('product',p)} onClick={() => setEdit(true)}>edit</button>
-                        </div></div>
+                                <button
+                                    className={CartCss.editButton}
+                                    onMouseOver={() => cookie.set('product', p)}
+                                    onClick={() => setEdit(true)}
+                                >edit</button>
+
+                            </div>
+                        </div>
 
                         <div className={CartCss.buttonImg}>
                             <div className={CartCss.imgDiv}>
-                                <img src={require(`../img/${p.image}`)} alt={'drink'} className={CartCss.img}></img>
+                                <img
+                                    src={require(`../img/${p.image}`)}
+                                    alt={'drink'}
+                                    className={CartCss.img} />
                             </div>
+
                             <div>
 
                                 <div>price: $ {Number.parseFloat(p.price * p.quantity).toFixed(2)}</div>
                                 <div>quantity: {p.quantity}</div>
+
                             </div>
+
                         </div>
+
                     </div> : null
+
                 ) : <div>nothing in cart</div>}
             </form>
         </div>
