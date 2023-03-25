e92cafa1dd5b76b5d1e56a1873c1dd77133d32d5
Robert Rosenzweig <robgrosenzweig@gmail.com>
2023-03-25 15:07:58 -0400

    Update server file for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,3 +23,13 @@ app.use('/api', router);
 app.listen(PORT, () => {
   console.log(`Server is listening on:${PORT}`);
 });
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
