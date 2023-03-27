e2a223f31551e6837dc0063a86deed1dcfab75d9
Robert Rosenzweig <robgrosenzweig@gmail.com>
2023-03-26 20:50:18 -0400

    Update server file for deployment 7

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/grace-shopper';
+const connectionString = process.env.DATABASE_URL;
 
 const client = new Pool({
   connectionString,
