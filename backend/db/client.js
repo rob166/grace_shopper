b6943d2cc33e8e04c0cae285a3e6230218b2bc15
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-04-04 20:32:13 -0400

    data base

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config()
-const connectionString = process.env.EXTDATABASE_URL || 'postgres://danny:R7EnHA3Gs4BB4sZysnsNuUOyalXocjmm@dpg-cgmbk4g7osl9vto83130-a.ohio-postgres.render.com/grace_shopper_z0l9?ssl=true';
+const connectionString = process.env.EXTDATABASE_URL || 'postgres://danny:R7EnHA3Gs4BB4sZysnsNuUOyalXocjmm@dpg-cgmbk4g7osl9vto83130-a.ohio-postgres.render.com/grace_shopper_z0l9';
 // 'https://localhost:5432/grace-shopper';
 
 const client = new Pool({
