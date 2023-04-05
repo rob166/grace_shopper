7358b1cc81e9d5963365c3e5a11016edb427d1cf
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-04-04 20:23:25 -0400

    database on render added to connection sting

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config()
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/grace-shopper';
+const connectionString = process.env.EXTDATABASE_URL || 'postgres://danny:R7EnHA3Gs4BB4sZysnsNuUOyalXocjmm@dpg-cgmbk4g7osl9vto83130-a.ohio-postgres.render.com/grace_shopper_z0l9?ssl=true';
+// 'https://localhost:5432/grace-shopper';
 
 const client = new Pool({
   connectionString,
